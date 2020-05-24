3d6796f8da30f44e99967e2f788d1c689aad0dd7
misohan <michal.hanusniak@aiesec.cz>
2020-05-25 00:45:24 +0200

    Implement overriden methods in class OfficeManager

--- src/main/java/codecool/human/officeworkers/OfficeManager.java
+++ src/main/java/codecool/human/officeworkers/OfficeManager.java
@@ -1,4 +1,14 @@
 package codecool.human.officeworkers;
 
-public class OfficeManager {
+import codecool.human.Human;
+
+public class OfficeManager implements Human {
+
+    public String playAnInstrument() {
+        return "Tu turutu!";
+    }
+
+    public String sayThing() {
+        return null;
+    }
 }
