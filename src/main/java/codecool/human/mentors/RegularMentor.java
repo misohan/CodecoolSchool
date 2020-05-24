0f29ad0db807db0ae113801f82e5446265426f10
misohan <michal.hanusniak@aiesec.cz>
2020-05-25 00:40:44 +0200

    Create class RegularMentor and implement overriden methods

---
+++ src/main/java/codecool/human/mentors/RegularMentor.java
@@ -0,0 +1,14 @@
+package codecool.human.mentors;
+
+public class RegularMentor extends Mentors{
+
+    @Override
+    public String playAnInstrument() {
+        return super.playAnInstrument();
+    }
+
+    @Override
+    public String sayThing() {
+        return "Did you Google it?";
+    }
+}
