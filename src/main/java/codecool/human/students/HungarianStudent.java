05946dcd6a4bb1a139c5e5be4e8b39a5207741dc
misohan <michal.hanusniak@aiesec.cz>
2020-05-25 11:02:24 +0200

    Implement overridden methods for HungarianStudent and PolishStudent class

--- src/main/java/codecool/human/students/HungarianStudent.java
+++ src/main/java/codecool/human/students/HungarianStudent.java
@@ -1,4 +1,14 @@
 package codecool.human.students;
 
-public class HungarianStudent {
+public class HungarianStudent extends Students{
+
+    @Override
+    public String playAnInstrument() {
+        return super.playAnInstrument();
+    }
+
+    @Override
+    public String sayThing() {
+        return "egesz kerek";
+    }
 }
--- src/main/java/codecool/human/students/PolishStudent.java
+++ src/main/java/codecool/human/students/PolishStudent.java
@@ -1,4 +1,14 @@
 package codecool.human.students;
 
-public class PolishStudent {
+public class PolishStudent extends Students {
+
+    @Override
+    public String playAnInstrument() {
+        return super.playAnInstrument();
+    }
+
+    @Override
+    public String sayThing() {
+        return "I'd like to drink a beer";
+    }
 }
